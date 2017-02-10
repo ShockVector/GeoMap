5e28f87d12bc646a15600ce8403a6b6426192f85
stephenwest470 <stephenwest470@gmail.com>
2017-02-09 20:20:46 -0700

    Update milestone_4.js

--- js/milestone_4.js
+++ js/milestone_4.js
@@ -1,14 +1,10 @@
 function initialize() {
-	var seattle = new google.maps.LatLng(47.6062,-122.3321);
+	var us = new google.maps.LatLng(39.8282,-98.5795);
 	var mapOptions = {
-		zoom: 9,
-		center: seattle,
-		mapTypeId: google.maps.MapTypeId.HYBRID
-	};
-	var map = new google.maps.Map(
-		document.getElementById("map_canvas"),
-		mapOptions);
-  [
+		zoom: 4,
+		center: us,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: [
   {
     "featureType": "administrative",
     "elementType": "geometry",
@@ -154,4 +150,8 @@ function initialize() {
     ]
   }
 ]
+	};
+	var map = new google.maps.Map(
+		document.getElementById("map_canvas"),
+		mapOptions);	
 }
