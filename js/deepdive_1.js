72b51eba0409b86ec4ce24f94e547ad7d13c06f3
stephenwest470 <stephenwest470@gmail.com>
2017-02-16 19:48:07 -0700

    Add files via upload

--- js/deepdive_1.js
+++ js/deepdive_1.js
@@ -1,4 +1,12 @@
 function initMap() {
+	var shinjuku = new google.maps.LatLng(35.6906,139.6996);
+	var ikebukuro = new google.maps.LatLng(35.7289,139.7103);
+	var shibuya = new google.maps.LatLng(35.6580,139.7016);
+	var yokohama = new google.maps.LatLng(35.4658,139.6223);
+	var tokyo = new google.maps.LatLng(35.680833,139.766944);
+	var shinagawa = new google.maps.LatLng(35.6285,139.7388);
+	var takadanobaba = new google.maps.LatLng(35.7134,139.7041);
+	var shimbashi = new google.maps.LatLng(35.6661,139.7584);
     var map = new google.maps.Map(document.getElementById('map_canvas'), {
         center: {lat: 35.6895, lng: 139.6917},
         zoom: 12,
@@ -355,5 +363,67 @@ function initMap() {
     ]
   }
 ]
-        });
-      }
+});
+
+	var shinjukuMarker = new google.maps.Marker({
+		position: shinjuku,
+		title:"Shinjuku Station: 3.64 million passengers carried daily"
+		});
+	shinjukuMarker.setMap(map);
+	var ikebukuroMarker = new google.maps.Marker({
+		position: ikebukuro,
+		title:"Ikebukuro Station: 2.71 million passengers carried daily"
+		});
+	ikebukuroMarker.setMap(map);
+	var shibuyaMarker = new google.maps.Marker({
+		position: shibuya,
+		title:"Shibuya Station: 2.18 million passengers carried daily"
+		});
+	shibuyaMarker.setMap(map);
+	var yokohamaMarker = new google.maps.Marker({
+		position: yokohama,
+		title:"Yokohama Station: 2.09 million passengers carried daily"
+		});
+	yokohamaMarker.setMap(map);
+	var tokyoMarker = new google.maps.Marker({
+		position: tokyo,
+		title:"Tokyo Station: 1.12 million passengers carried daily"
+		});
+	tokyoMarker.setMap(map);
+	var shinagawaMarker = new google.maps.Marker({
+		position: shinagawa,
+		title:"Shinagawa Station: 0.91 million passengers carried daily"
+		});
+	shinagawaMarker.setMap(map);
+	var takadanobabaMarker = new google.maps.Marker({
+		position: takadanobaba,
+		title:"Takadanobaba Station: 0.90 million passengers carried daily"
+		});
+	takadanobabaMarker.setMap(map);
+	var shimbashiMarker = new google.maps.Marker({
+		position: shimbashi,
+		title:"Shimbashi Station: 0.85 million passengers carried daily"
+		});
+	shimbashiMarker.setMap(map);
+
+
+	var shinjukuInfoContent = '<div class="infoBox">' +
+		'<p>Shinjuku Station: 3.64 million passengers carried daily</p>' +
+		'</div>'
+	var shinjukuInfoWindow = new google.maps.InfoWindow({
+		content: shinjukuInfoContent
+		});
+	google.maps.event.addListener(shinjukuMarker, 'click', function() {
+		classInfoWindow.open(map,shinjukuMarker);
+		});
+	var ikebukuroInfoContent = '<div class="infoBox">' +
+		'<p>Ikebukuro Station: 2.71 million passengers carried daily</p>' +
+		'<iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1486322485343!6m8!1m7!1sneDc4DwioOJ-TytixzvJEg!2m2!1d35.08459518161192!2d-106.6243050674837!3f207.5327084691508!4f-4.850942482843806!5f0.7820865974627469" width="300" height="225" frameborder="0" style="border:0" allowfullscreen></iframe>' +
+		'</div>'
+	var ikebukuroInfoWindow = new google.maps.InfoWindow({
+		content: ikebukuroInfoContent
+		});
+	google.maps.event.addListener(ikebukuroMarker, 'click', function() {
+		classInfoWindow.open(map,ikebukuroMarker);
+		});
+}
--- js/milestone_3.js
+++ js/milestone_3.js
@@ -1,17 +1,16 @@
 function initialize() {
 	var seattle = new google.maps.LatLng(47.6062,-122.3321);
-	var redmond = new google.maps.LatLng(47.6740,-122.1215);
-	var mapOptions = {
+	var redmond = new google.maps.LatLng(47.6740,-122.1215);	
+	var map = new google.maps.Map(
+		document.getElementById("map_canvas"),{
 		zoom: 9,
 		center: seattle,
 		mapTypeId: google.maps.MapTypeId.HYBRID
-	};
-	var map = new google.maps.Map(
-		document.getElementById("map_canvas"),
-		mapOptions);
+	});
+
 	var redmondMarker = new google.maps.Marker({
 		position: redmond,
 		title:"My Birth City"
 		});
-	marker.setMap(map);
+	redmondMarker.setMap(map);
 }
