8987fcd4efb6c978ba4dda02b0ee672207b8a7f1
stephenwest470 <stephenwest470@gmail.com>
2018-10-10 19:24:26 -0600

    Update milestone_3.js

--- js/milestone_3.js
+++ js/milestone_3.js
@@ -1,14 +1,13 @@
 function initialize() {
-	var seattle = new google.maps.LatLng(47.6062,-122.3321);
-	var redmond = new google.maps.LatLng(47.6740,-122.1215);	
+	var student1 = new google.maps.LatLng(47.6062,-122.3321);
+	var student2 = new google.maps.LatLng(47.6740,-122.1215);	
 	var map = new google.maps.Map(
 		document.getElementById("map_canvas"),{
-		zoom: 9,
-		center: seattle,
+		zoom: 1,
 		mapTypeId: google.maps.MapTypeId.HYBRID
 	});
 
-	var redmondMarker = new google.maps.Marker({
+	var student1Marker = new google.maps.Marker({
 		position: redmond,
 		title:"My Birth City"
 		});
